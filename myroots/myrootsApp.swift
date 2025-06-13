90ac3368c2eebb5ac87a89a9825fbd962029c329
Emiliano Montes <101071655+louieliet@users.noreply.github.com>
2025-06-13 14:57:41 -0600

    Initial Commit

---
+++ myroots/myrootsApp.swift
@@ -0,0 +1,17 @@
+//
+//  myrootsApp.swift
+//  myroots
+//
+//  Created by Emiliano Montes on 13/06/25.
+//
+
+import SwiftUI
+
+@main
+struct myrootsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ myrootsTests/myrootsTests.swift
@@ -0,0 +1,17 @@
+//
+//  myrootsTests.swift
+//  myrootsTests
+//
+//  Created by Emiliano Montes on 13/06/25.
+//
+
+import Testing
+@testable import myroots
+
+struct myrootsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
